c3f1ab00dcc789980c18d807f6ffb1640aa7781f
BasileKocheida <basilesimplon@gmail.com>
2022-09-26 09:39:42 +0200

    version avec input pour iframe

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,41 @@
+const divP = document.querySelectorAll('p');
+const hr = document.querySelectorAll('hr');
+const divContent = document.querySelector('.content');
+const nav = document.querySelector('nav');
+const brand = document.querySelector('.brand');
+// let btn = document.querySelector('#btn');
+let btn = document.querySelector('#flexSwitchCheckDefault');
+
+let isClicked = false
+
+btn.addEventListener('click', () => {
+    if(isClicked == false){
+        isClicked = true
+        divContent.classList.replace("content", "content-darkmode");
+        nav.classList.add("content-darkmode");
+        btn.classList.replace("btn-dark", "btn-light");
+        divP.forEach(p=>{
+            p.classList.add("p-darkmode");
+        })
+        hr.forEach(h=>{
+            h.classList.add("p-darkmode");
+        })
+    }else{
+        isClicked = false
+        divContent.classList.replace("content-darkmode", "content");
+        nav.classList.remove("content-darkmode");
+        btn.classList.replace("btn-light", "btn-dark");
+        divP.forEach(p=>{
+            p.classList.remove("p-darkmode");
+        })
+        hr.forEach(h=>{
+            h.classList.remove("p-darkmode");
+        })
+
+    }
+})
+
+
 //lance la video//
 function save() {
     let iframe = document.getElementById("iframeCode").value
@@ -30,4 +68,5 @@ window.onload = function load() {
         })
     })
 }
+
 
